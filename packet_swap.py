81e7512e2b8f06c700aa25d4e6ebb14ccb1ea9b7
Caleb Morales <caleb.morales@brightbridgeweb.com>
2021-02-28 13:42:28 -0700

    got basic movement, started on win condition

--- packet_swap.py
+++ packet_swap.py
@@ -17,31 +17,30 @@ class game_state:
             x, y, turn, opponent = 1, 0, 'O', 'X'
         else:
             x, y, turn, opponent = 0, 1, 'X', 'O'
-
         if packet[0] == 4 or packet[1] == 4:
             return self
-
         destination = (packet[0]+x, packet[1]+y)
         hop_over = (packet[0]+(x*2), packet[1]+(y*2))
-
         if self.game_board[destination] == opponent and self.game_board[hop_over] == opponent:
             return self
-
         if self.game_board[destination] == ' ':
             self.game_board[destination] = turn
             self.game_board[packet] = ' '
             self.player_turn = opponent
-
         if self.game_board[destination] == opponent:
             if self.game_board[hop_over] == ' ':
                 self.game_board[hop_over] = turn
                 self.game_board[packet] = ' '
                 self.player_turn = opponent
-
         return self
-        
-        
 
-def new_game() -> game_state:
-    game = game_state()
-    return game
+    def check_win_condition(self):
+        x_win_condition = [(1,4), (2,4), (3,4)]
+        o_win_condition [(4,1), (4,2), (4,3)]
+        if self.player_turn == 'X':
+            if all(self.game_board[i] == self.player_turn for i in x_win_condition):
+                return true
+        if self.player_turn == 'O':
+            if all(self.game_board == self.player_turn for i in o_win_condition):
+                return turn
+        return false
